13b9465e8ae04840a167e4aafbc2e55bbcb77080
CowSaysBaa <mememe479@gmail.com>
2020-10-14 16:22:46 +0800

    V0.2 complete, require refactoring

--- server/server.js
+++ server/server.js
@@ -1,27 +1,47 @@
 const express = require("express")
 const app = express()
 const { pool } = require("./dbConfig")
+const session = require("express-session")
+const flash = require("express-flash")
+const passport = require("passport")
+const initializePassport = require("./passportConfig")
+
+initializePassport(passport)
 
 const PORT = process.env.PORT || 8080
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/users/login', (req, res) => {
+app.get('/users/login', checkAuthenticated, (req, res) => {
     res.render('login')
 })
 
-app.get('/users/register', (req, res) => {
+app.get('/users/logout', (req, res) => {
+    req.logOut()
+    req.flash('success_msg', 'You have logged out')
+    res.redirect('/users/login')
+})
+
+app.get('/users/register', checkAuthenticated, (req, res) => {
     res.render('register')
 })
 
-app.get('/users/dashboard', (req, res) => {
-    res.render('dashboard')
+app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
+    res.render("dashboard", { user: req.user.username });
 })
 
 app.post('/users/register', async (req, res) => {
@@ -57,13 +77,52 @@ app.post('/users/register', async (req, res) => {
           [username],
           (err, results) => {
             if (err) {
-              console.log(err);
+              console.log(err)
             }
-            console.log(results.rows);
+            console.log(results.rows)
+            
+            if(results.rows.length > 0) {
+                errors.push({message: "The username is already taken"})
+                res.render('register', { errors })
+            } else {
+                pool.query(
+                    `INSERT INTO users (username, password)
+                    VALUES ($1, $2)
+                    RETURNING username, password`, [username, password], (err, results) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                        console.log(results.rows)
+                        req.flash('success_msg', "You are now registered. Please log in")
+                        res.redirect('/users/login')
+                    }
+                )
+            }
+
             }
         )
     }
 })
+
+app.post('/users/login', passport.authenticate('local', {
+    successRedirect: '/users/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+}))
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/users/dashboard')
+    }
+    next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/users/login')
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
