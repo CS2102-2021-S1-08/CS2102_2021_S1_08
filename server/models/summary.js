a25e2922acbeac623bb82d9e8d524104f1c0ce05
leeweiminsg <lee.wei.min@u.nus.edu>
2020-10-31 18:33:22 +0800

    Clean up summary

--- server/models/summary.js
+++ server/models/summary.js
@@ -1,31 +1,21 @@
 /**
  * Model for monthly summaries
- */ 
+ */
 const { pool } = require("../dbConfig");
 
-// TODO SQL QUERIES
-// CREATE TABLE 
-
 // GET
-function getMonthlySummary(ctname, year, month) {
-    // async/await
-    const query = 'SELECT * \
+async function getMonthlySummary(ctname, year, month) {
+  // async/await
+  const query =
+    "SELECT * \
                     FROM monthly_summary \
-                    WHERE ctname=$1, year=$2, month=$3'
-    const values = [ctname, year, month]
+                    WHERE ctname=$1::text AND year=$2::int AND month=$3::int";
+  const values = [ctname, year, month];
 
-    try {
-        const res = await client.query(query, values)
-        console.log(res.rows[0])
-    } catch (err) {
-        console.log(err.stack)
-    }
+  try {
+    const res = await pool.query(query, values);
+    console.log(res.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+  }
 }
-
-// POST
-// function createMonthlySummary() {}
-
-// DELETE
-// function deleteMonthlySummary(ctname, year, month) {
-    
-// }
