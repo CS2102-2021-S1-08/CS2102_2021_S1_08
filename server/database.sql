089af04c17174aa1f5df4a112b7afa6e3a673887
alexwxh <e0273907@u.nus.edu>
2020-10-31 15:02:45 +0800

    schema update

--- server/database.sql
+++ server/database.sql
@@ -3,50 +3,21 @@ CREATE TABLE IF NOT EXISTS users (
 	password varchar(200) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Petowner (
+CREATE TABLE IF NOT EXISTS pet_owner (
     username varchar(200) REFERENCES users(username)
     PRIMARY KEY (username)
 );
 
-CREATE TABLE IF NOT EXISTS Pet (
+CREATE TABLE IF NOT EXISTS pet (
 	pname  varchar(200),
-	username varchar(200) REFERENCES Petowner(username),
+	username varchar(200) REFERENCES pet_owner(username),
 	password varchar(200) NOT NULL,
 	profile varchar(200),
-	category varchar(50) REFERENCES PetCategory(category),
+	category varchar(50) REFERENCES pet_category(category),
 	special varchar(200),
 	PRIMARY KEY (pname, username)
 );
 
-CREATE TABLE IF NOT EXISTS PetCategory (
+CREATE TABLE IF NOT EXISTS pet_category (
     category varchar(200) PRIMARY KEY NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS Bids (
-	start_date date,
-	end_date date,
-	category varchar(50),
-	pname varchar(50) REFERENCES Pet(pname) ON DELETE CASCADE,
-	bid_start_date date NOT NULL,
-	bid_end_date date NOT NULL,
-	review varchar,
-	rating int CHECK((rating IS NULL) OR (rating >= 0 AND RATING <= 5)),
-	successful boolean NOT NULL
-	FOREIGN KEY (start_date, end_date, category) REFERENCES 
-		Availability(start_date, end_date, category) ON DELETE CASCADE,
-	PRIMARY KEY (start_date, end_date, category, pname)
-);
-
-CREATE TABLE IF NOT EXISTS BasePrices (
-	category varchar(50) PRIMARY KEY,
-	price int NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS MonthlySummary (
-  ctname varchar(50) REFERENCES CareTaker (name) ON DELETE cascade,
-  year INT,
-  month INT,
-  petDays INT,
-  salary INT,
-  PRIMARY KEY(ctname, year, month)
-)
