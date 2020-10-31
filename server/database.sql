537eba76aa6a458adfb1e431d17c06f2a588370e
gohyeeloon <goh.yeeloon@gmail.com>
2020-10-31 15:13:43 +0800

    Update tables to follow lower snake case

--- server/database.sql
+++ server/database.sql
@@ -3,27 +3,27 @@ CREATE TABLE IF NOT EXISTS users (
 	password varchar(200) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Bids (
+CREATE TABLE IF NOT EXISTS bids (
 	start_date date,
 	end_date date,
 	category varchar(50),
-	pname varchar(50) REFERENCES Pet(pname) ON DELETE CASCADE,
+	pname varchar(50) REFERENCES pets(pname) ON DELETE CASCADE,
 	bid_start_date date NOT NULL,
 	bid_end_date date NOT NULL,
 	review varchar,
-	rating int CHECK((rating IS NULL) OR (rating >= 0 AND RATING <= 5)),
+	rating int CHECK((rating IS NULL) OR (rating >= 0 AND rating <= 5)),
 	successful boolean NOT NULL
 	FOREIGN KEY (start_date, end_date, category) REFERENCES 
-		Availability(start_date, end_date, category) ON DELETE CASCADE,
+		availability(start_date, end_date, category) ON DELETE CASCADE,
 	PRIMARY KEY (start_date, end_date, category, pname)
 );
 
-CREATE TABLE IF NOT EXISTS BasePrices (
+CREATE TABLE IF NOT EXISTS base_prices (
 	category varchar(50) PRIMARY KEY,
 	price int NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS MonthlySummary (
+CREATE TABLE IF NOT EXISTS onthlySummary (
   ctname varchar(50) REFERENCES CareTaker (name) ON DELETE cascade,
   year INT,
   month INT,
