548f662ce3c54d07ecc2090dd333bdae51a01cb7
leeweiminsg <lee.wei.min@u.nus.edu>
2020-10-31 14:05:14 +0800

    Add monthly summary

--- server/database.sql
+++ server/database.sql
@@ -2,3 +2,12 @@ CREATE TABLE IF NOT EXISTS users (
 	username  varchar(200) PRIMARY KEY NOT NULL,
 	password varchar(200) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS MonthlySummary (
+  ctname varchar(50) REFERENCES CareTaker (name) ON DELETE cascade,
+  year INT,
+  month INT,
+  petDays INT,
+  salary INT,
+  PRIMARY KEY(year, month)
+)
