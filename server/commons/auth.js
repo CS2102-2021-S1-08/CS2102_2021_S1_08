d83d716cf668cf67f1e334a56016fea027ff80de
leeweiminsg <lee.wei.min@u.nus.edu>
2020-10-31 19:01:34 +0800

    Fix table name bug

--- server/commons/auth.js
+++ server/commons/auth.js
@@ -1,58 +1,61 @@
 /**
- * Collection of authentication middleware 
+ * Collection of authentication middleware
  */
 
-const { pool } = require("../dbConfig")
+const { pool } = require("../dbConfig");
 
 exports.checkAuthenticated = function (req, res, next) {
   if (req.isAuthenticated()) {
-      return res.redirect('/users/dashboard')
+    return res.redirect("/users/dashboard");
   }
-  return next()
-}
+  return next();
+};
 
 exports.checkNotAuthenticated = function (req, res, next) {
   if (req.isAuthenticated()) {
-      return next()
+    return next();
   }
-  res.redirect('/users/login')
-}
+  res.redirect("/users/login");
+};
 
 exports.checkNotAuthenticatedAsAdmin = function (req, res, next) {
-  pool.query(`SELECT * FROM pcs_admins WHERE username = $1::text`,
+  pool.query(
+    `SELECT * FROM pcs_admins WHERE username = $1::text`,
     [req.user],
     (err, res) => {
       if (results.rows.length > 0) {
-        next()
+        next();
       } else {
-        res.render('error', { message: 'Not authenticated as Admin' })
+        res.render("error", { message: "Not authenticated as Admin" });
       }
     }
-  )
-}
+  );
+};
 
 exports.checkNotAuthenticatedAsCareTaker = function (req, res, next) {
-  pool.query(`SELECT * FROM pcs_admins WHERE username = $1::text`,
+  pool.query(
+    `SELECT * FROM care_takers WHERE username = $1::text`,
     [req.user],
     (err, res) => {
       if (results.rows.length > 0) {
-        next()
+        next();
       } else {
-        res.render('error', { message: "Not authenticated as Admin" })
+        res.render("error", { message: "Not authenticated as care taker" });
       }
     }
-  )
-}
+  );
+};
 
 exports.checkNotAuthenticatedAsPetOwner = function (req, res, next) {
-  pool.query(`SELECT * FROM pcs_admins WHERE username = $1::text`,
+  pool.query(
+    `SELECT * FROM pet_owners WHERE username = $1::text`,
     [req.user],
     (err, res) => {
       if (results.rows.length > 0) {
-        next()
+        next();
       } else {
-        res.render('error', { message: "Not authenticated as Admin" })
+        res.render("error", { message: "Not authenticated as pet owner" });
       }
     }
-  )
-}
+  );
+};
