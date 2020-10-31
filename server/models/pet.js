cf27415d3647848f8887f35d6e71746a844dc022
alexwxh <e0273907@u.nus.edu>
2020-10-31 17:44:13 +0800

    pet queries

--- server/models/pet.js
+++ server/models/pet.js
@@ -6,5 +6,61 @@
 // CREATE TABLE 
 
 // GET
+function getAllPets(username) {
+    let result = []
+
+    pool.query(`
+    SELECT pname, profile, category, special_requirements
+    FROM pets
+    WHERE username = $1::text
+    `,
+        [username],
+        (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack)
+            }
+            result = res;
+        })
+
+    return result
+}
+
 // POST
+function createAvailability(username, pname, profile, category, special_requirements) {
+    let result = false
+
+    pool.query(`
+    INSERT INTO pets
+    VALUES ($1::text, $2::text, $3::text, $4::text, $5::text)
+    `,
+        [username, pname, profile, category, special_requirements],
+        (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack)
+            } else {
+                result = true
+            }
+        })
+
+    return result
+}
+
 // DELETE
+function createAvailability(username, pname) {
+    let result = false
+
+    pool.query(`
+    DELETE FROM pets
+    WHERE (username = $1::text AND pname = $2::pname)
+    `,
+        [username, pname],
+        (err, res) => {
+            if (err) {
+                console.error('Error executing query', err.stack)
+            } else {
+                result = true
+            }
+        })
+
+    return result
+}
