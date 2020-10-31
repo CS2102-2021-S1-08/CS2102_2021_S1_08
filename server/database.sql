186180aaea3b83e96272e3ef99d04a7797ad152a
Shuming <geshuming2@gmail.com>
2020-10-31 16:42:07 +0800

    Cleanup sql

--- server/database.sql
+++ server/database.sql
@@ -75,12 +75,3 @@ CREATE TABLE IF NOT EXISTS monthly_summary (
 	salary INT CHECK(salary >= 0),
 	PRIMARY KEY(ctname, year, month)
 )
-
-CREATE TABLE IF NOT EXISTS pet (
-	pname  varchar(200),
-	username varchar(200) REFERENCES pet_owners(username),
-	profile varchar(200),
-	category varchar(50) REFERENCES base_prices(category),
-	special varchar(200),
-	PRIMARY KEY (pname, username)
-);
