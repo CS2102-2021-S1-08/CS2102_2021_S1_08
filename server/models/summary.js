42a0c1312d7765dd92e0e0cb0cd79a5a4979cea3
leeweiminsg <lee.wei.min@u.nus.edu>
2020-10-31 15:16:37 +0800

    Add getsummary

--- server/models/summary.js
+++ server/models/summary.js
@@ -6,5 +6,25 @@
 // CREATE TABLE 
 
 // GET
+function getMonthlySummary(ctname, year, month) {
+    // async/await
+    const query = 'SELECT * \
+                    FROM monthly_summary \
+                    WHERE ctname=$1, year=$2, month=$3'
+    const values = [ctname, year, month]
+
+    try {
+        const res = await client.query(query, values)
+        console.log(res.rows[0])
+    } catch (err) {
+        console.log(err.stack)
+    }
+}
+
 // POST
+// function createMonthlySummary() {}
+
 // DELETE
+// function deleteMonthlySummary(ctname, year, month) {
+    
+// }
