0f57ef9fb7e58fd5adccafabe6922ba54f37f6bc
gohyeeloon <goh.yeeloon@gmail.com>
2020-10-31 14:31:34 +0800

    Fix minor changes in bid table

--- server/database.sql
+++ server/database.sql
@@ -3,24 +3,24 @@ CREATE TABLE IF NOT EXISTS users (
 	password varchar(200) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS bids (
+CREATE TABLE IF NOT EXISTS Bids (
 	start_date date,
 	end_date date,
-	category varchar(200),
-	pname varchar(200) REFERENCES pet(pname) ON DELETE CASCADE,
+	category varchar(50),
+	pname varchar(50) REFERENCES Pet(pname) ON DELETE CASCADE,
 	bid_start_date date NOT NULL,
 	bid_end_date date NOT NULL,
 	review varchar,
-	rating decimal(2,1),
+	rating int CHECK((rating IS NULL) OR (rating >= 0 AND RATING <= 5)),
 	successful boolean NOT NULL
 	FOREIGN KEY (start_date, end_date, category) REFERENCES 
-		availability(start_date, end_date, category) ON DELETE CASCADE,
+		Availability(start_date, end_date, category) ON DELETE CASCADE,
 	PRIMARY KEY (start_date, end_date, category, pname)
 );
 
-CREATE TABLE IF NOT EXISTS base_prices (
-	category varchar(200) PRIMARY KEY,
-	price decimal(5,2) NOT NULL
+CREATE TABLE IF NOT EXISTS BasePrices (
+	category varchar(50) PRIMARY KEY,
+	price int NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS MonthlySummary (
