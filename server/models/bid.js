af18f3d71ca757db75f917d4688684f092f7bfc8
gohyeeloon <goh.yeeloon@gmail.com>
2020-11-01 23:58:19 +0800

    implement methods for bid router

--- server/models/bid.js
+++ server/models/bid.js
@@ -5,13 +5,13 @@
 const { pool } = require("../dbConfig");
 
 // GET BID
-function getBid(start_date, end_date, category, poname, pname, ctuname) {
+function getBidAsPetOwner(poname) {
   return pool.query(`
     SELECT *
     FROM bids
-    WHERE start_date = $1::date, end_date = $2::date, category = $3::text, poname = $4::text, pname = $5::text, ctuname = $6::text
+    WHERE poname = $1::text
     `,
-    [start_date, end_date, category, poname, pname, ctuname]
+    [poname]
   )
 }
 
@@ -36,50 +36,61 @@ function deleteBid(start_date, end_date, category, poname, pname, ctuname) {
 }
 
 // DELETE REVIEW
-function deleteReview(poname, pname, ctuname, start_date, end_date, category) {
+function deleteReview(start_date, end_date, category, poname, pname, ctuname) {
   return pool.query(`
     UPDATE bids
     SET review = null
-    WHERE poname = $1::text, pname = $2::pname, ctuname = $3::text, start_date = $4::date, end_date = $5::date, category = $6::text
+    WHERE start_date = $1::date, end_date = $2::date, category = $3::text, poname = $4::text, pname = $5::text, ctuname = $6::text
     `,
     [start_date, end_date, category, poname, pname, ctuname]
   )
 }
 
 // DELETE RATING
-function deleteRating(poname, pname, ctuname, start_date, end_date, category) {
+function deleteRating(start_date, end_date, category, poname, pname, ctuname) {
   return pool.query(`
     UPDATE bids
     SET rating = null
-    WHERE poname = $1::text, pname = $2::pname, ctuname = $3::text, start_date = $4::date, end_date = $5::date, category = $6::text
+    WHERE start_date = $1::date, end_date = $2::date, category = $3::text, poname = $4::text, pname = $5::text, ctuname = $6::text
     `,
     [start_date, end_date, category, poname, pname, ctuname]
   )
 }
 
 // UPDATE REVIEW
-function updateReview(poname, pname, ctuname, start_date, end_date, category, review) {
+function updateReview(start_date, end_date, category, poname, pname, ctuname, review) {
   return pool.query(`
     UPDATE bids
     SET review = $7::text
-    WHERE poname = $1::text, pname = $2::pname, ctuname = $3::text, start_date = $4::date, end_date = $5::date, category = $6::text
+    WHERE start_date = $1::date, end_date = $2::date, category = $3::text, poname = $4::text, pname = $5::text, ctuname = $6::text
     `,
     [start_date, end_date, category, poname, pname, ctuname, review]
   )
 }
 
 // UPDATE RATING
-function updateRating(poname, pname, ctuname, start_date, end_date, category, rating) {
+function updateRating(start_date, end_date, category, poname, pname, ctuname, rating) {
   return pool.query(`
     UPDATE bids
     SET rating = $7::int
-    WHERE poname = $1::text, pname = $2::pname, ctuname = $3::text, start_date = $4::date, end_date = $5::date, category = $6::text
+    WHERE start_date = $1::date, end_date = $2::date, category = $3::text, poname = $4::text, pname = $5::text, ctuname = $6::text
     `,
     [start_date, end_date, category, poname, pname, ctuname, rating]
   )
 }
 
-exports.getBid = getBid;
+// MARK BID AS SUCCESSFUL
+function markBidAsSuccessful(start_date, end_date, category, poname, pname, ctuname) {
+  return pool.query(`
+    UPDATE bids
+    SET is_successful = true
+    WHERE start_date = $1::date, end_date = $2::date, category = $3::text, poname = $4::text, pname = $5::text, ctuname = $6::text
+    `,
+    [start_date, end_date, category, poname, pname, ctuname]
+  )
+}
+
+exports.getBidAsPetOwner = getBidAsPetOwner;
 exports.createBid = createBid;
 exports.deleteBid = deleteBid;
 exports.updateRating = updateRating;
--- server/routes/bid.js
+++ server/routes/bid.js
@@ -3,30 +3,189 @@
  */ 
 
 const router = require("express").Router()
+const {
+    checkAuthenticatedAsCareTaker,
+    checkAuthenticatedAsPetOwner,
+  } = require("../commons/auth");
+  const {
+    getBidAsPetOwner,
+    createBid,
+    deleteBid,
+    updateRating,
+    deleteRating,
+    updateReview,
+    deleteReview,
+    markBidAsSuccessful
+  } = require("../models/bid");
 
 // TODO: use middleware checkAuthenticatedAsPetOwner
 // TODO: Pet owner can get all bids they have made (GET)
-const getBid = function (req,res) {
-
-}
+async function getBidsAsPetOwner(req, res) {
+    let results = await getBidAsPetOwner(req.user["username"]);
+  
+    res.render("bidsMade", {
+        user: req.user["username"],
+        data: results,
+    });
+  }
 
 // TODO: Pet owner can place a bid (POST)
-const postBid = function (req, res) {
+const placeBidAsPetOwner = function (req, res) {
+    let results = await createBid(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname, 
+        new Date(req.body.bid_start_date), 
+        new Date(req.body.bid_end_date)
+    );
 
+    res.render("bidPlaced", {
+        user: req.user["username"],
+        data: results,
+    });
 }
 
 // TODO: Pet owner can delete a bid (DELETE)
-const deleteBid = function (req, res) {
+const deleteBidAsPetOwner = function (req, res) {
+    let results = await deleteBid(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname
+    );
 
+    res.render("bidDeleted", {
+        user: req.user["username"],
+        data: results,
+    });
 }
 
 // TODO: Pet owner can add a review/rating
+const addReviewAsPetOwner = function (req, res) {
+    let results = await updateReview(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname,
+        req.body.review
+    );
+
+    res.render("reviewAdded", {
+        user: req.user["username"],
+        data: results,
+    });
+}
+
+const addRatingAsPetOwner = function (req, res) {
+    let results = await updateRating(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname,
+        req.body.rating
+    );
+
+    res.render("reviewModified", {
+        user: req.user["username"],
+        data: results,
+    });
+}
+
 // TODO: Pet owner can modify a review/rating
+const modifyReviewAsPetOwner = function (req, res) {
+    let results = await updateReview(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname,
+        req.body.review
+    );
+
+    res.render("reviewModified", {
+        user: req.user["username"],
+        data: results,
+    });
+}
+
+const modifyRatingAsPetOwner = function (req, res) {
+    let results = await updateRating(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname,
+        req.body.rating
+    );
+
+    res.render("ratingModified", {
+        user: req.user["username"],
+        data: results,
+    });
+}
+
 // TODO: Pet owner can delete a review/rating
+const deleteReviewAsPetOwner = function (req, res) {
+    let results = await deleteReview(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname,
+        req.body.review
+    );
+
+    res.render("reviewModified", {
+        user: req.user["username"],
+        data: results,
+    });
+}
+
+const deleteRatingAsPetOwner = function (req, res) {
+    let results = await deleteRating(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname,
+        req.body.rating
+    );
+
+    res.render("ratingModified", {
+        user: req.user["username"],
+        data: results,
+    });
+}
 
 // TODO: use middleware checkAuthenticaedAsCareTaker
 // TODO: Care Taker can accept a bid (PUT)
+const acceptBidAsCareTaker = function (req, res) {
+    let results = await markBidAsSuccessful(
+        new Date(req.body.start_date), 
+        new Date(req.body.end_date), 
+        req.body.category, 
+        req.user.username, 
+        req.body.pname, 
+        req.body.ctuname
+    );
 
-
+    res.render("bidMarkedAsSuccessful", {
+        user: req.user["username"],
+        data: results,
+    });
+}
 
 module.exports = router;
