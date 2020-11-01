5f5848bb7e183dbcf96717e044c819b70952d0ed
Shuming <geshuming2@gmail.com>
2020-11-01 18:25:46 +0800

    Add CRUD for availability

--- server/models/availability.js
+++ server/models/availability.js
@@ -38,7 +38,16 @@ function createAvailability(username, start_date, end_date, category) {
 }
 
 // DELETE
+function deleteAvailabilty(username, start_date, end_date, category) {
+  return pool.query(`
+    DELETE FROM availabilities
+    WHERE username = $1::text AND start_date = $2::date AND end_date = $3::date AND category = $4::text
+  `,
+  [username, start_date, end_date, category]
+  )
+}
 // TODO
 
-exports.createAvailability = createAvailability;
-exports.getAllAvailabilities = getAllAvailabilities;
+exports.createAvailability = createAvailability
+exports.getAllAvailabilities = getAllAvailabilities
+exports.deleteAvailabilty = deleteAvailabilty
--- server/routes/availability.js
+++ server/routes/availability.js
@@ -1,12 +1,58 @@
 /**
  * Router for availabilities 
- */ 
+ */
+const { getAllAvailabilities, deleteAvailabilty, createAvailability } = require("../models/availability");
 
 const router = require("express").Router()
 
-// TODO: use middleware checkAuthenticatedAsCareTaker
-// TODO: Care Taker can view their availabilities (GET)
-// TODO: Care Taker can add new availabilities (POST)
-// TODO: Care Taker can remove availabilities (DELETE)
+function getController(req, res) {
+  getAllAvailabilities(req.user.username)
+    .then(data => {
+      res.render('availability', { user: req.user.username, data: data.rows })
+    })
+    .catch(err => {
+      res.render('error', { error: err })
+    });
+}
+
+function postController(req, res) {
+  createAvailability(req.user.username, new Date(req.body.start_date), new Date(req.body.end_date)  , req.body.category)
+    .then(data => {
+      getAllAvailabilities(req.user.username)
+        .then(data => {
+          res.render('availability', { user: req.user.username, data: data.rows })
+        })
+        .catch(err => {
+          res.render('error', { message: "Error", error: err })
+      })
+    })
+    .catch(err => {
+      res.render('error', { message: "Error",error: err })
+    })
+}
+
+function deleteController(req, res) {
+  deleteAvailabilty(req.user.username, new Date(req.body.start_date), new Date(req.body.end_date), req.body.category)
+    .then(data => {
+      getAllAvailabilities(req.user.username)
+        .then(data => {
+          res.render('availability', { user: req.user.username, data: data.rows })
+        })
+        .catch(err => {
+          res.render('error', { message: "Error", error: err })
+      })
+    })
+    .catch(err => {
+      res.render('error', { message: "Error", error: err })
+    })
+}
+
+// TODO: Add authentication middleware NOT A PRIORITY
+router.route("/")
+  .get(getController)
+  .post(postController);
+
+router.route("/delete")
+  .post(deleteController);
 
 module.exports = router;
